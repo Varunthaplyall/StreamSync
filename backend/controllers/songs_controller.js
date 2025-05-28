188b2b736fbc07a1ca3f45f64cc484a56644fc85
varunt-rapidquest <Varunthapliyal70@gmail.com>
2025-05-28 17:57:47 +0530

    wip

--- backend/controllers/songs_controller.js
+++ backend/controllers/songs_controller.js
@@ -5,7 +5,7 @@ export default {
         try {
             const req = await axios.get('https://api.spotify.com/v1/me/tracks', {
                 headers: {
-                    'Authorization':`Bearer ${process.env.SPOTIFY_SECRET_TOKEN}`
+                    'Authorization':`Bearer BQDI1l9r4jvDaxy3r7ziGelKYDroxrbEPu2u5rnObqt6b6lDrpU-EzcfR6xF7te6CebEZzrH1Ql-QK8RX2NtYHu2Mpvu7XPDYGwXXZkkf0aKTdRzcfZj1StVdn7sa_araezadiIXfqT1Eo3n0249bs3arjLwqKAMW7Z0T6PemMGbw3epkxXkC6xJuSMJnBbK_CcSpyKxldco_dCk32Tu5ahh3WBcD60Q3-FLU95CB-NqxNYvHM-aW8U0MzyWSVJsEOg2xcKG`
                 },
                 params:{
                     limit: 50,
@@ -14,7 +14,7 @@ export default {
             })
 
             console.log(req)
-            req.status(200).send("hello")
+            res.status(200).send("hello")
         } catch (error) {
             console.log(error)
         }
